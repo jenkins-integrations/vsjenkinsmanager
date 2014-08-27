31fc5a40fb2b5eb057bebe181c4d336c2389ba4b
Tom Kuijsten <tomkuijsten@users.noreply.github.com>
2014-08-27 13:35:06 +0200

    Updated to version 0.2

--- Devkoes.VSJenkinsManager/Solution Items/SharedAssemblyInfo.cs
+++ Devkoes.VSJenkinsManager/Solution Items/SharedAssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -22,5 +21,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.*")]
+[assembly: AssemblyVersion("0.2.*")]
+[assembly: AssemblyFileVersion("0.2.*")]
