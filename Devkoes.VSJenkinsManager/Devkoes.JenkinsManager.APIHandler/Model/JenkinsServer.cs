49c4b06c8b45d33a68e9d7080aeafc137a1dbde9
MartinWeijsenfeld <martin.josina@kpnplanet.nl>
2014-09-27 15:59:41 +0200

    Made JenkinsServer class observable.

--- Devkoes.VSJenkinsManager/Devkoes.JenkinsManager.APIHandler/Model/JenkinsServer.cs
+++ Devkoes.VSJenkinsManager/Devkoes.JenkinsManager.APIHandler/Model/JenkinsServer.cs
@@ -1,11 +1,61 @@
 ﻿
 namespace Devkoes.JenkinsManager.APIHandler.Model
 {
-    public class JenkinsServer
+    public class JenkinsServer: ObservableObject
     {
-        public string Url { get; set; }
-        public string Name { get; set; }
-        public string UserName { get; set; }
-        public string ApiToken { get; set; }
+        private string _Url;
+        private string _Name;
+        private string _UserName;
+        private string _ApiToken;
+        
+        public string Url 
+        {
+            get { return _Url; }
+            set
+            {
+                if (value != _Url)
+                {
+                    _Url = value;
+                    RaisePropertyChanged(() => Url);
+                }
+            }
+        }
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                if (value != _Name)
+                {
+                    _Name = value;
+                    RaisePropertyChanged(() => Name);
+                }
+            }
+        }
+        public string UserName
+        {
+            get { return _UserName; }
+            set
+            {
+                if (value != _UserName)
+                {
+                    _UserName = value;
+                    RaisePropertyChanged(() => UserName);
+                }
+            }
+        }
+        public string ApiToken
+        {
+            get { return _ApiToken; }
+            set
+            {
+                if (value != _ApiToken)
+                {
+                    _ApiToken = value;
+                    RaisePropertyChanged(() => ApiToken);
+                }
+            }
+        }
+
     }
 }
