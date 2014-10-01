7e1de3f656dd63d8c5bb5f5d3058dc54dfad54f8
Tom Kuijsten <devkoes@outlook.com>
2014-10-01 21:45:13 +0200

    Added progressbar Disable views combobox when loading

---
+++ Devkoes.VSJenkinsManager/Devkoes.JenkinsManager.UI/Converters/BooleanInverterConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Devkoes.JenkinsManager.UI.Converters
+{
+    class BooleanInverterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (!(value is bool))
+            {
+                throw new ArgumentException("Convert parameter should be a boolean");
+            }
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
