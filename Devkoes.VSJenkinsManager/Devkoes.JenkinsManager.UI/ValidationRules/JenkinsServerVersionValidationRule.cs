33494ca0896374f2163fcb8dc97cd94822d74572
Tom Kuijsten <tomkuijsten@hotmail.com>
2014-10-16 16:41:17 +0200

    Added basic validation setup

---
+++ Devkoes.VSJenkinsManager/Devkoes.JenkinsManager.UI/ValidationRules/JenkinsServerVersionValidationRule.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Windows.Controls;
+namespace Devkoes.JenkinsManager.UI.ValidationRules
+{
+    public class JenkinsServerVersionValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            return new ValidationResult(false, "Always invalid");
+        }
+    }
+}
